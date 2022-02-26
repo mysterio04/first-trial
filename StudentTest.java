205b7d908dc5c7714c808a51e9b0b8d734e431d6
jainish solanki <96974544+mysterio04@users.noreply.github.com>
2022-02-27 00:44:08 +0530

    StudentTest.java updated

--- StudentTest.java
+++ StudentTest.java
@@ -1,83 +1,75 @@
-
-
 import java.util.Scanner;
 
-public class StudentTest {
-
+public class trial1 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         Student S1 = new Student();
         Student S2 = new Student();
 
         System.out.println("enter the name of student 1");
-        Scanner sc = new Scanner(System.in);
-        String S1name = sc.nextLine();
+        String n1 = sc.nextLine();
+        S1.setName(n1);
 
 
         System.out.println("enter Roll number of student 1");
-        Scanner sc1 = new Scanner(System.in);
-        int S1RollNo = sc1.nextInt();
-
+        int r1 = sc.nextInt();
+        S1.setRollNo(r1);
 
         System.out.println("enter the marks of student 1");
-        Scanner sc2 = new Scanner(System.in);
-        float S1marks = sc2.nextFloat();
-        if(S1marks<0)
-        {
-            S1marks=0;
-        }
+        float m1 = sc.nextFloat();
+        S1.setMarks(m1);
+
 
 
         System.out.println("enter the attendance of student 1 in percentage");
-        Scanner sc3 = new Scanner(System.in);
-        int S1attendance = sc3.nextInt();
-        if(S1attendance>75)
+        int a1 = sc.nextInt();
+        S1.setAttendance(a1);
+        if(a1>75)
         {
-            S1marks=(S1marks)+S1marks/10;
+            m1=(m1)+m1/10;
         }
 
-
         System.out.println("enter the name of student 2");
-        Scanner sc4 = new Scanner(System.in);
-        String S2name = sc4.nextLine();
-
+        String n2 = sc.next();
+        S2.setName(n2);
 
         System.out.println("enter the Roll number of student 2");
-        Scanner sc5 = new Scanner(System.in);
-        int S2RollNo = sc5.nextInt();
-
+        int r2 = sc.nextInt();
+        S2.setRollNo(r2);
 
         System.out.println("enter the marks of student 2");
-        Scanner sc6 = new Scanner(System.in);
-        float S2marks = sc6.nextFloat();
-        if(S2marks<0)
+        float m2 = sc.nextFloat();
+        S2.setMarks(m2);
+        if(m2<0)
         {
-            S2marks=0;
+            m2=0;
         }
 
 
         System.out.println("enter the attendance of student 2 in percentage");
-        Scanner sc7 = new Scanner(System.in);
-        int S2attendance = sc7.nextInt();
+
+        int S2attendance = sc.nextInt();
         if(S2attendance>75)
         {
-            S2marks=(S2marks)+S2marks/10;
+            m2=(m2)+m2/10;
         }
 
 
-         if(S1marks>100)
-         {
-             S1marks=100;
-         }
-         if (S2marks>100)
-         {
-             S1marks = 100;
-         }
+        if(m1>100)
+        {
+            m1=100;
+        }
+        if (m2>100)
+        {
+            m2 = 100;
+        }
 
-        System.out.printf("the marks of %s are ", S1name);
-        System.out.println(S1marks);
-        System.out.printf("the marks of %s are ", S2name);
-        System.out.println(S2marks);
+        System.out.printf("the marks of %s are ", S1.getName());
+        System.out.println(m1);
+        System.out.printf("the marks of %s are ", S2.getName());
+        System.out.println(m2);
 
 
 
@@ -93,4 +85,46 @@ class Student{
     int RollNo;
     float marks;
     int attendance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getMarks() {
+        return marks;
+    }
+
+    public void setMarks(float marks) {
+        if(marks<0)
+        {
+            marks=0;
+        }
+        else if(marks >100 )
+        {
+            marks = 100;
+        }
+        this.marks = marks;
+    }
+
+    public int getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(int attendance) {
+        this.attendance = attendance;
+    }
+
+    public int getRollNo() {
+        return RollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        RollNo = rollNo;
+    }
 }
+
+
